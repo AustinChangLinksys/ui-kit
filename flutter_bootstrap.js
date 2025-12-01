c9e20c8b920b5e4523bc12469213409ae8323872
AustinChangLinksys <AustinChangLinksys@users.noreply.github.com>
2025-12-01 08:52:21 +0000

    deploy: 78ad5784ff8130a20272fb2490af9734a07602a5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1726660505"
+    serviceWorkerVersion: "2136391588"
   }
 });
