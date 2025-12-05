c7f762046156221d57788a6aab49b6beb7e149cd
Austin Chang <austin.chang@belkin.com>
2025-12-05 20:59:27 +0800

    feat(theme): Implement AppStyleColors and fix fixed colors usage
    - Introduced `AppStyleColors` theme extension for style adaptability (Pixel, Glass, etc.)
    - Integrated `AppStyleColors` into `AppDesignTheme`
    - Refactored all design styles (Brutal, Flat, Glass, Neumorphic, Pixel) to use `AppStyleColors` and semantic colors from `AppPalette` or `ColorScheme` instead of hardcoded `Colors.*`
    - Updated `AppPalette` with semantic color definitions (success, warning, grey scale)
    - Manually updated `AppDesignTheme` tailor mixin to include `styleColors`

--- lib/src/foundation/theme/design_system/app_design_theme.tailor.dart
+++ lib/src/foundation/theme/design_system/app_design_theme.tailor.dart
@@ -43,6 +43,7 @@ mixin _$AppDesignThemeTailorMixin on ThemeExtension<AppDesignTheme> {
   CarouselStyle get carouselStyle;
   ChipGroupStyle get chipGroupStyle;
   TopologySpec get topologySpec;
+  AppStyleColors get styleColors;
 
   @override
   AppDesignTheme copyWith({
@@ -79,6 +80,7 @@ mixin _$AppDesignThemeTailorMixin on ThemeExtension<AppDesignTheme> {
     CarouselStyle? carouselStyle,
     ChipGroupStyle? chipGroupStyle,
     TopologySpec? topologySpec,
+    AppStyleColors? styleColors,
   }) {
     return AppDesignTheme(
       toggleStyle: toggleStyle ?? this.toggleStyle,
@@ -114,6 +116,7 @@ mixin _$AppDesignThemeTailorMixin on ThemeExtension<AppDesignTheme> {
       carouselStyle: carouselStyle ?? this.carouselStyle,
       chipGroupStyle: chipGroupStyle ?? this.chipGroupStyle,
       topologySpec: topologySpec ?? this.topologySpec,
+      styleColors: styleColors ?? this.styleColors,
     );
   }
 
@@ -156,6 +159,7 @@ mixin _$AppDesignThemeTailorMixin on ThemeExtension<AppDesignTheme> {
       carouselStyle: carouselStyle.lerp(other.carouselStyle, t),
       chipGroupStyle: chipGroupStyle.lerp(other.chipGroupStyle, t),
       topologySpec: topologySpec.lerp(other.topologySpec, t),
+      styleColors: styleColors.lerp(other.styleColors, t),
     );
   }
 
@@ -224,7 +228,9 @@ mixin _$AppDesignThemeTailorMixin on ThemeExtension<AppDesignTheme> {
             const DeepCollectionEquality()
                 .equals(chipGroupStyle, other.chipGroupStyle) &&
             const DeepCollectionEquality()
-                .equals(topologySpec, other.topologySpec));
+                .equals(topologySpec, other.topologySpec) &&
+            const DeepCollectionEquality()
+                .equals(styleColors, other.styleColors));
   }
 
   @override
@@ -264,6 +270,7 @@ mixin _$AppDesignThemeTailorMixin on ThemeExtension<AppDesignTheme> {
       const DeepCollectionEquality().hash(carouselStyle),
       const DeepCollectionEquality().hash(chipGroupStyle),
       const DeepCollectionEquality().hash(topologySpec),
+      const DeepCollectionEquality().hash(styleColors),
     ]);
   }
 }
@@ -305,4 +312,5 @@ extension AppDesignThemeBuildContextProps on BuildContext {
   CarouselStyle get carouselStyle => appDesignTheme.carouselStyle;
   ChipGroupStyle get chipGroupStyle => appDesignTheme.chipGroupStyle;
   TopologySpec get topologySpec => appDesignTheme.topologySpec;
+  AppStyleColors get styleColors => appDesignTheme.styleColors;
 }
